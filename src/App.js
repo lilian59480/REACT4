babacf0210796a6b42dbb285445c8ac90fab9ac3
Thibaut <thibautfenain1@gmail.com>
2019-05-10 09:25:39 +0200

    add material

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Task from './Task/Task';
+import Manager from './Manager/Manager';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-function App () {
+class App extends Component {
+	state = {
+		open: false
+	};
+
+	handleOpen = () => {
+		this.setState({ open: true });
+	};
+
+	handleClose = () => {
+		this.setState({ open: false });
+	  };
+	
+	  render () {
 	return (
 		<div className="App">
-			<Task title="Test" description="Desc" beginDate={new Date()} endDate={new Date()} />
-			<Task title="Test2" description="Desc" beginDate={new Date()} endDate={new Date()} />
-			<Task title="Test3" description="Desc" beginDate={new Date()} endDate={new Date()} />
+			<Button onClick={this.handleOpen}>Open Modal</Button>
+			<Modal
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+				open={this.state.open}
+				onClose={this.handleClose}
+        	>
+							<Manager />
+
+			</Modal>
+			<Task
+				title="Test"
+				description="Desc"
+				beginDate={new Date()}
+				endDate={new Date()}
+			/>
+			<Task
+				title="Test2"
+				description="Desc"
+				beginDate={new Date()}
+				endDate={new Date()}
+			/>
+			<Task
+				title="Test3"
+				description="Desc"
+				beginDate={new Date()}
+				endDate={new Date()}
+			/>
 		</div>
 	);
+	}
 }
 
 export default App;
---
+++ src/Manager/Manager.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class Manager extends Component {
+	render () {
+		return (
+			<Button variant="contained" color="primary">
+        Create Task
+			</Button>
+		);
+	}
+}
+
+export default Manager;
