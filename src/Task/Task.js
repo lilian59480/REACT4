90c7cb2798a476121f49a2930559cddad2862045
Lilian Petitpas <lilian.petitpas@outlook.com>
2019-05-10 09:46:09 +0200

    [TASK] Update Tasks

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -6,7 +6,6 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import './Task.css';
 
 /**
  * Un titre
