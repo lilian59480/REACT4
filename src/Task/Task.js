46eebd8f31e962bf59f2cb91aa52bac5d43a8205
Lilian Petitpas <lilian.petitpas@outlook.com>
2019-05-10 09:02:09 +0200

    [SRC] Add Task class

---
+++ src/Task/Task.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+import './Task.css';
+
+/**
+ * Un titre
+ * Une description
+ * Date et heure de début
+ * Date et heure de fin
+ *
+ */
+class Task extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		const { title, description, beginDate, endDate } = this.props;
+
+		return (
+			<div>
+				<h1>{title}</h1>
+				<p>{description}</p>
+				<p>{beginDate.toUTCString()}</p>
+				<p>{endDate.toUTCString()}</p>
+			</div>
+		);
+	}
+}
+
+Task.propTypes = {
+	title: Proptypes.string,
+	description: Proptypes.string,
+	beginDate: Proptypes.instanceOf(Date),
+	endDate: Proptypes.instanceOf(Date)
+};
+
+export default Task;
